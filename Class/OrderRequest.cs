93c2ba4ff78facc0099167284ceac5a1fc886d5a
DingoPham <pencibambo@gmail.com>
2025-03-01 15:48:44 +0700

    update 3 more controller to move img and video function

--- Class/OrderRequest.cs
+++ Class/OrderRequest.cs
@@ -5,5 +5,8 @@ namespace ArtworkCore.Class
     public class OrderRequest
     {
         public List<SfwArt> Images { get; set; }
+        public List<NsfwArt> NsfwImages { get; set; }
+        public List<Video> Videos { get; set; }
+        public List<NsfwVideo> NsfwVideos { get; set; }
     }
 }
